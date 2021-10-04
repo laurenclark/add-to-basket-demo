26dd02e02924e0fa46e2e34a1741067bf29f5d5e
Lauren Clark <lauren.c4231@gmail.com>
2021-10-04 01:18:14 +0100

    Revert "Refactor for types and useQuery"
    This reverts commit eb1effae6c0e6c3dbf4ee23594603e417b195234.

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,35 +3,63 @@ import Head from "next/head";
 import Image from "next/image";
 import Navigation from "../components/Navigation";
 import Footer from "../components/Footer";
-import { gql, useQuery } from "@apollo/client";
+import { gql } from "@apollo/client";
+import styled from "styled-components";
+import client from "../lib/apolloClient";
 import dummyImage from "../public/images/dummy-bottle.png";
-import { GetProducts } from "./__generated__/GetProducts";
-import { Main, Grid, ProductCard, Button } from "./indexStyles";
 
-const FetchProductsWithCurrency = gql`
-    query fetchProductsWithCurrency {
-        products {
-            id
-            name
-            nutrients
-            price
-        }
-        currency {
-            id
+const Main = styled.main`
+    margin: 2rem auto 4rem;
+    max-width: 90vw;
+    width: 1200px;
+    min-height: 50vh;
+    h1 {
+        @media screen and (min-width: 992px) {
+            max-width: 450px;
+            font-size: 2.5vw;
+            line-height: 1.2;
         }
     }
 `;
 
-const Home: NextPage = () => {
-    const { data, loading, error } = useQuery<GetProducts>(
-        FetchProductsWithCurrency
-    );
+const Grid = styled.div`
+    display: grid;
+    grid-template-columns: repeat(auto-fit, minmax(200px, 2fr));
+    grid-gap: 1rem;
+`;
 
-    if (error) {
-        console.error(error);
-        return <div>Error!</div>;
+const ProductCard = styled.div`
+    padding: 1.3em;
+    background: white;
+    box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
+    transition: 0.3s;
+`;
+
+const Button = styled.button`
+    position: relative;
+    height: 50px;
+    width: 100%;
+    align-items: center;
+    justify-content: center;
+    padding: 0 20px;
+    min-width: 160px;
+    cursor: pointer;
+    transition: 0.2s all cubic-bezier(0.25, 0.46, 0.45, 0.94);
+    display: inline-flex;
+    padding-left: 15px;
+    padding-right: 15px;
+    font-size: 18px;
+    background-color: #ffd326;
+    border: none;
+    &:hover {
+        color: #333;
+        box-shadow: 0 0.4px 0.4px rgb(0 0 0 / 2%), 0 1px 1px rgb(0 0 0 / 3%),
+            0 2.1px 2.1px rgb(0 0 0 / 4%), 0 4.4px 4.4px rgb(0 0 0 / 5%),
+            0 12px 12px rgb(0 0 0 / 7%);
     }
+`;
 
+const Home: NextPage = ({ products, currency }: any) => {
     return (
         <>
             <Head>
@@ -49,25 +77,23 @@ const Home: NextPage = () => {
             <Main>
                 <h1>Live life better with personalised nutrition</h1>
                 <Grid>
-                    {loading && <div>Loading...</div>}
-                    {data &&
-                        data!.products.map((product: any) => (
-                            <ProductCard key={product!.id}>
-                                <strong>{product!.name}</strong> <br />
-                                <Image
-                                    blurDataURL="../images/dummy-bottle-loader.png"
-                                    placeholder="blur"
-                                    width="189"
-                                    height="189"
-                                    src={dummyImage}
-                                />
-                                {product!.price.toLocaleString("en-gb", {
-                                    style: "currency",
-                                    currency: data.currency[0].id
-                                })}
-                                <Button>Add to Basket</Button>
-                            </ProductCard>
-                        ))}
+                    {products.map((product: any) => (
+                        <ProductCard key={product!.id}>
+                            <strong>{product!.name}</strong> <br />
+                            <Image
+                                blurDataURL="../images/dummy-bottle-loader.png"
+                                placeholder="blur"
+                                width="189"
+                                height="189"
+                                src={dummyImage}
+                            />
+                            {product.price.toLocaleString("en-gb", {
+                                style: "currency",
+                                currency: currency[0].id
+                            })}
+                            <Button>Add to Basket</Button>
+                        </ProductCard>
+                    ))}
                 </Grid>
             </Main>
             <Footer />
@@ -76,3 +102,27 @@ const Home: NextPage = () => {
 };
 
 export default Home;
+
+export async function getServerSideProps() {
+    const { data } = await client.query({
+        query: gql`
+            query GetProducts {
+                products {
+                    id
+                    name
+                    nutrients
+                    price
+                }
+                currency {
+                    id
+                }
+            }
+        `
+    });
+    return {
+        props: {
+            products: data.products,
+            currency: data.currency
+        }
+    };
+}
