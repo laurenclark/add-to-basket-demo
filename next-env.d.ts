d3306bcca4dcb8eca1c1df0cdfd9d9cef093f24f
Lauren Clark <lauren.c4231@gmail.com>
2021-10-04 01:18:09 +0100

    Revert "Revert"
    This reverts commit 03e59722efa1e5ee7fd677451eee0123b75f38a9.

--- next-env.d.ts
+++ next-env.d.ts
@@ -4,3 +4,16 @@
 
 // NOTE: This file should not be edited
 // see https://nextjs.org/docs/basic-features/typescript for more information.
+import { CSSProp } from "styled-components";
+
+declare module "styled-components" {
+    export interface DefaultTheme {
+        // Your theme stuff here
+    }
+}
+
+declare module "react" {
+    interface Attributes {
+        css?: CSSProp;
+    }
+}
