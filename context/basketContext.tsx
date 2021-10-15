5f368959e23273df93a9a7caf2e918a843942a47
Lauren Clark <lauren.c4231@gmail.com>
2021-10-15 16:36:26 +0100

    Remove mess and start to type the context

--- context/basketContext.tsx
+++ context/basketContext.tsx
@@ -1,19 +1,29 @@
-import { useState, useEffect, createContext, FC } from "react";
+import { useState, createContext, FC } from "react";
 import { useToggle } from "../hooks/useToggle";
 import { gql, useQuery } from "@apollo/client";
 import { toast } from "react-toastify";
 
 interface BasketContext {
-    product: {
-        id: string;
-    };
     isHidden: Boolean;
+    changeProductQuantity: () => number;
+    removeProductFromBasket: () => void;
+    getTotalQuantities: () => number;
+    setIsHidden: () => boolean;
+    setProductsInBasket: () => void | [];
 }
-
 interface Nutrients {
     id: string;
     amount: number;
 }
+
+interface Product {
+    id: string;
+    name: string;
+    nutrients: [];
+    quantity?: number | any;
+}
+
+type ProductsInBasket = [{ Product: Product }];
 
 const BasketContext = createContext<Partial<BasketContext>>({});
 
@@ -24,66 +34,6 @@ const BasketProvider: FC = ({ children }) => {
 
     if (error) {
         console.error(error);
-    }
-
-    function changeProductQuantity(
-        product: {},
-        plusOrMinus: string = "plus",
-        quantity: number = 1
-    ) {
-        const newProductArray = [...productsInBasket];
-        const isSKUInBasket = newProductArray.find(
-            // @ts-ignore
-            (basketProduct: any) => basketProduct.name == product.name
-        );
-
-        if (isSKUInBasket) {
-            switch (plusOrMinus) {
-                case "minus":
-                    // @ts-ignore
-                    if (isSKUInBasket.quantity > 1) {
-                        // @ts-ignore
-                        isSKUInBasket.quantity =
-                            // @ts-ignore
-                            isSKUInBasket.quantity - quantity;
-                    }
-                    break;
-                default:
-                    if (validateTUL(newProductArray).length === 0) {
-                        // @ts-ignore
-                        isSKUInBasket.quantity =
-                            // @ts-ignore
-                            isSKUInBasket.quantity + quantity;
-                    }
-                    break;
-            }
-        } else {
-            // @ts-ignore
-            if (validateTUL(newProductArray).length === 0) {
-                // @ts-ignore
-                newProductArray.push({ ...product, quantity });
-            }
-        }
-        if (validateTUL(newProductArray).length === 0) {
-            console.log("New product");
-            return newProductArray;
-        } else {
-            toast.error(
-                `You have exceeded the maximum amounts of: ${validateTUL(
-                    newProductArray
-                )}`,
-                {
-                    position: "top-center",
-                    autoClose: 5000,
-                    hideProgressBar: false,
-                    closeOnClick: true,
-                    pauseOnHover: true,
-                    draggable: true,
-                    progress: undefined
-                }
-            );
-            return productsInBasket;
-        }
     }
 
     function removeProductFromBasket(productId: string) {
@@ -115,11 +65,10 @@ const BasketProvider: FC = ({ children }) => {
 
         function comparisonArray() {
             var newArr: [] = [];
-            TULData.tolerableUpperLimits.forEach((tul: any) => {
-                combineDuplicateNutrients.forEach((nutrient: any) => {
+            TULData.tolerableUpperLimits.forEach((tul: Nutrients) => {
+                combineDuplicateNutrients.forEach((nutrient: Nutrients) => {
                     if (tul.id === nutrient.id) {
                         if (nutrient.amount >= tul.amount) {
-                            // @ts-ignore
                             newArr.push(nutrient.id);
                         }
                     }
@@ -132,7 +81,6 @@ const BasketProvider: FC = ({ children }) => {
 
     function getTotalQuantities(productsArray = productsInBasket) {
         return productsArray.reduce((accumulator, current) => {
-            // @ts-ignore
             return accumulator + current!.quantity;
         }, 0);
     }
@@ -140,7 +88,6 @@ const BasketProvider: FC = ({ children }) => {
     return (
         <BasketContext.Provider
             value={{
-                // @ts-ignore
                 changeProductQuantity,
                 removeProductFromBasket,
                 getTotalQuantities,
